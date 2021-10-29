7b6fd429b2599e069171e2961609cdcb87af57da
Riday ♡ <ridayshah2001@gmail.com>
2021-10-29 19:43:54 +0530

    🗃️ Chore: use localstorage instead of db

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import Layout from '../components/Layout';
 import Link from '../components/Link';
 
+type UserType = 'candidate' | 'employer';
+
+export interface User {
+	name: string;
+	email: string;
+	type: UserType;
+}
+
 const Register: NextPage = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [type, setType] = useState<UserType>('candidate');
+	const router = useRouter();
+
 	return (
 		<Layout>
 			<main className='w-1/2 mx-auto rounded-3xl shadow-md p-9'>
@@ -20,16 +34,29 @@ const Register: NextPage = () => {
 					className='w-2/3 mx-auto flex justify-evenly flex-wrap pt-12'
 					onSubmit={e => {
 						e.preventDefault();
-						console.log('Form Submitted!');
+						if (typeof window !== undefined) {
+							const accounts: Array<User> = JSON.parse(window.localStorage.getItem('accounts') || '[]');
+							if (accounts.find(a => a.email !== email)) {
+								accounts.push({ name, email, type });
+								window.localStorage.setItem('accounts', JSON.stringify(accounts));
+							}
+							router.push('/login');
+						}
 					}}
 				>
 					<div className='w-full flex justify-between items-center py-3'>
 						<label htmlFor='name'>Name</label>
-						<input type='text' id='name' className='input' required />
+						<input type='text' id='name' className='input' required onBlur={e => setName(e.target.value)} />
 					</div>
 					<div className='w-full flex justify-between items-center py-3'>
 						<label htmlFor='email'>Email</label>
-						<input type='email' id='email' className='input' required />
+						<input
+							type='email'
+							id='email'
+							className='input'
+							required
+							onBlur={e => setEmail(e.target.value)}
+						/>
 					</div>
 					<div className='w-full flex justify-between items-center py-3'>
 						<label htmlFor='password'>New Password</label>
@@ -48,8 +75,12 @@ const Register: NextPage = () => {
 					<div className='w-full flex justify-between items-center py-3'>
 						<label htmlFor='user-type'>Select user type:</label>
 						<select name='user-type' id='user-type' className='input py-2 px-3 bg-white' required>
-							<option value='candidate'>Candidate</option>
-							<option value='employer'>Employer</option>
+							<option value='candidate' onClick={() => setType('candidate')}>
+								Candidate
+							</option>
+							<option value='employer' onClick={() => setType('employer')}>
+								Employer
+							</option>
 						</select>
 					</div>
 
